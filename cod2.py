c334fa16c308df492242ea0763e97c9b3a028de7
robinnn20 <113219415+robinnn20@users.noreply.github.com>
2024-08-28 22:08:58 -0400

    Update cod2.py

--- cod2.py
+++ cod2.py
@@ -2,13 +2,18 @@ from scapy.all import *
 import time
 
 def send_icmp_message(target_ip, message):
+    # Se define el tamaño total del campo de datos en bytes (48 bytes)
+    data_length = 48
+    
     for char in message:
-        # Crear el paquete ICMP con el carácter en el campo de datos
-        packet = IP(dst=target_ip)/ICMP()/char
+        data = char + (' ' * (data_length - len(char)))
+        # Crear el paquete ICMP con el campo de datos de longitud 48 bytes
+        packet = IP(dst=target_ip)/ICMP()/data
         # Enviar el paquete
         send(packet, verbose=0)
         # Esperar un breve período para evitar generar demasiado tráfico
         time.sleep(0.1)
+    
     print("Mensaje enviado con éxito.")
 
 if __name__ == "__main__":
