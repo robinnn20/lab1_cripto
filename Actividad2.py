5b7dc9789add5745da91e63251454fc9098d651f
robinnn20 <113219415+robinnn20@users.noreply.github.com>
2024-08-30 18:41:13 -0400

    Update Actividad2.py

--- Actividad2.py
+++ Actividad2.py
@@ -3,22 +3,17 @@ import time
 
 def send_icmp_message(target_ip, message):
     data_length = 48  # Tamaño total del campo de datos en bytes
-    packet_id = random.randint(0, 65535)  # Generar un ID de paquete ICMP aleatorio
+    packet_id = 0  
     seq_number = 0  # Número de secuencia inicial
-    
     for char in message:
-        # Crear el contenido del campo de datos con el carácter y rellenar el resto con espacios
-        data = char + (' ' * (data_length - len(char)))
-        
-        # Crear el paquete ICMP con el ID y número de secuencia específicos
-        packet = IP(dst=target_ip)/ICMP(id=packet_id, seq=seq_number)/data
-        
+        data = char + (' ' * (data_length - len(char))) 
+        # Se crea el paquete ICMP con el ID y número de secuencia específicos
+        packet = IP(dst=target_ip)/ICMP(id=packet_id, seq=seq_number)/data  
         # Enviar el paquete
-        send(packet, verbose=0)
-        
+        send(packet, verbose=0) 
         # Incrementar el número de secuencia para el próximo paquete
         seq_number += 1
-        
+        packet_id +=1
         # Esperar un breve período para evitar generar demasiado tráfico
         time.sleep(0.1)
     
